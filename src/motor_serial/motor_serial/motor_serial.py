785481a9edf7f454eb7c3a9ab75f956729604bfa
raspberry <lanthier@kth.se>
2025-12-03 11:37:52 +0100

    Two stm!

--- src/motor_serial/motor_serial/motor_serial.py
+++ src/motor_serial/motor_serial/motor_serial.py
@@ -40,13 +40,13 @@ class MotorSerialNode(Node):
 
         # --- Initialize both serial ports ---
         try:
-            self.ser1 = serial.Serial("/dev/ttyACM2", 115200, timeout=0.1)
+            self.ser1 = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
             self.ser1.flushInput()
             self.ser1.flushOutput()
 
-            #self.ser2 = serial.Serial("/dev/ttyACM1", 115200, timeout=0.1)
-            #self.ser2.flushInput()
-            #self.ser2.flushOutput()
+            self.ser2 = serial.Serial("/dev/ttyACM1", 115200, timeout=0.1)
+            self.ser2.flushInput()
+            self.ser2.flushOutput()
 
             time.sleep(1)
             self.get_logger().info("✅ Serial connected to STM32 on both ports!")
@@ -62,7 +62,8 @@ class MotorSerialNode(Node):
         self.motor_commands = bytearray([0]*16)  # 12 motors
 
         #####Setting the motor moving parameters
-        self.motors_idle = True #Are ALL motors running or available
+        self.motors_idle1 = True
+        self.motors_idle2 = True #Are ALL motors running or available
         self.pending_motor_commands = deque() #This is the queue of motor commands that gets sent when the motors are idling
 
         self.peak_detector = PeakDetector(min_peak_height=1)
@@ -88,8 +89,8 @@ class MotorSerialNode(Node):
         self.send_motor_commands()
 
     def float_to_byte(self, x):
-        #x = max(-127, min(127, int(x)))
-        return x + 0
+        x = max(-127, min(127, int(x)))
+        return x + 127
     
     def joystick_callback(self, msg):
         """Store latest joystick data."""
@@ -103,7 +104,13 @@ class MotorSerialNode(Node):
 
         if peak is not None:
             peak_byte = self.float_to_byte(peak)
-            peak_packet = bytearray([peak_byte] * 16)
+            peak_packet = (bytearray([
+            (int(msg.motor1_fr)), (int(msg.motor2_fr)), (int(msg.motor3_fr)),
+            (int(msg.motor1_fl)), (int(msg.motor2_fl)), (int(msg.motor3_fl)),
+            (int(msg.motor1_rr)), (int(msg.motor2_rr)), (int(msg.motor3_rr)),
+            (int(msg.motor1_rl)), (int(msg.motor2_rl)), (int(msg.motor3_rl)), 
+            self.float_to_byte(int(msg.step_fr)), self.float_to_byte((msg.step_fl)),
+            self.float_to_byte(int(msg.step_rr)), self.float_to_byte((msg.step_rl))])) #bytearray([peak_byte] * 16)
             self.pending_motor_commands.append(peak_packet)
             self.get_logger().info(f"PEAK DETECTED: {peak} → queued")
 
@@ -127,28 +134,15 @@ class MotorSerialNode(Node):
     def send_motor_commands(self):
         """Send motor commands to both serial devices and log sent/received bytes."""
         try:
-            # # Handle relay release timing
-            # if self.releasing and (time.time() - self.release_start_time > 2.0):
-            #     self.relay_commands = bytearray([10, 10])
-            #     self.releasing = False
-
-
-            # self.get_logger().info(f"We not in this bitch, motors idle: {self.motors_idle}")
-            # self.get_logger().info(f"Current queue length: {len(self.pending_motor_commands)}")
-
-            # if self.motors_idle and self.pending_motor_commands:
-            #     self.get_logger().info(f"Sike! We IN THIS BITCH, motors idle: {self.motors_idle}")
-            #     self.motor_commands = self.pending_motor_commands.popleft() #Pop out the next command in the queue
-
             
             if self.releasing and (time.time() - self.release_start_time > 2.0):
                 self.relay_commands = bytearray([10, 10])
                 self.releasing = False
 
-            self.get_logger().info(f"Motors idle: {self.motors_idle}")
+            self.get_logger().info(f"Motors idle: 1 - {self.motors_idle1}, 2 - {self.motors_idle2}")
             self.get_logger().info(f"Queue length before cleanup: {len(self.pending_motor_commands)}")
 
-            if self.motors_idle and self.pending_motor_commands:
+            if self.motors_idle1 and self.motors_idle2 and self.pending_motor_commands:
                 # --- Clean up the queue: if next command is zero, jump to latest zero ---
                 if all(b == 0 for b in self.pending_motor_commands[0]):
                     # find the last zero packet in the queue
@@ -169,12 +163,13 @@ class MotorSerialNode(Node):
             # Prepare data for STM1 (FR + RL DC motors + all servos)
             data1 = self.get_data_to_stm32(stm=1)
             # Prepare data for STM2 (FL + RR DC motors)
-            #data2 = self.get_data_to_stm32(stm=2)
+            data2 = self.get_data_to_stm32(stm=2)
 
             # --- Send to STM1 ---
             if data1:
                 with self.lock1:
                     self.get_logger().info(f"→ Sending ({len(data1)} bytes) to STM1: {[b for b in data1]}")
+                    self.get_logger().info(f"Expected encoder value: {self.motor_commands[3]*4*7*30}")
                     self.ser1.write(data1)
                     self.ser1.flush()
                     time.sleep(0.05)  # small delay
@@ -185,31 +180,21 @@ class MotorSerialNode(Node):
                     while len(response) < 16 and time.time() < timeout:
                         response += self.ser1.read(16 - len(response))
 
-                    #if len(response) == 16:
-                            #self.get_logger().info(f"← Echo ({len(response)} bytes) from STM1: {[b for b in response]}")
-                    # else:
-                    #    self.get_logger().warn(f"⚠️ Incomplete echo from STM1 ({len(response)} bytes)")
-
                     ##New debugging unpacking
                     if len(response) == 16:
                         if response[0] == ord('P'):
                             try:
                                 p1,p2,p3 = struct.unpack('<iii', response[1:13])
-                                # != 0 in the following is to make sure that we only say motors arent moving when its 0. If signal is noisy and sends something besides 0 or 1 its safer to assume the motor is moving
-                                #m1_moving = response[13] != 0 #Is motor1 moving
-                                #m2_moving = response[14] != 0 #Is motor2 moving
-                                #m3_moving = response[15] != 0 #Is motor3 moving
-
                                 m1_moving = response[13]
                                 m2_moving = response[14]
                                 m3_moving = response[15]
 
-                                self.motors_idle = not (m1_moving or m2_moving)
+                                self.motors_idle1 = not (m1_moving or m2_moving or m3_moving)
                                 
-                                self.get_logger().info(f"\nPosition: {p1}\nPosition 2: {p2}\nPosition 3: {p3}"
-                                                       f"\nM1 is moving: {m1_moving}"
-                                                       f"\nM2 is moving: {m2_moving}")
-                                                       #f"\nM3 is moving: {m3_moving}")
+                                self.get_logger().info(f"\n1 - position 1: {p1}\n1 - position 2: {p2}\n1 - position 3: {p3}"
+                                                       f"\nM1_1 is moving: {m1_moving}"
+                                                       f"\nM2_1 is moving: {m2_moving}"
+                                                       f"\nM3_1 is moving: {m3_moving}")
                             except struct.error as e:
                                 self.get_logger().warn(f"Failed to unpack positions: {e}")
                         else:
@@ -218,60 +203,45 @@ class MotorSerialNode(Node):
                         self.get_logger().warn(f"Incomplete response from STM1 ({len(response)} bytes)")
 
             # --- Send to STM2 ---
-            #if data2:
-            #    with self.lock2:
-            #        self.get_logger().info(f"→ Sending ({len(data2)} bytes) to STM2: {[hex(b) for b in data2]}")
-            #        self.ser2.write(data2)
-            #        self.ser2.flush()
-            #        time.sleep(0.05)
+            if data2:
+                with self.lock2:
+                    self.get_logger().info(f"→ Sending ({len(data2)} bytes) to STM2: {[b for b in data2]}")
+                    self.get_logger().info(f"Expected encoder value: {self.motor_commands[3]*4*7*30}")
+                    self.ser2.write(data2)
+                    self.ser2.flush()
+                    time.sleep(0.05)  # small delay
 
-            #        # Read 16-byte echo from STM2
-            #        response = bytearray()
-            #        timeout = time.time() + 0.5
-            #        while len(response) < 16 and time.time() < timeout:
-            #            response += self.ser2.read(16 - len(response))
+                    # Read 16-byte echo from STM1
+                    response2 = bytearray()
+                    timeout2 = time.time() + 0.5  # 0.5s timeout
+                    while len(response2) < 16 and time.time() < timeout2:
+                        response2 += self.ser2.read(16 - len(response2))
 
-            #        if len(response) == 16:
-            #            self.get_logger().info(f"← Echo ({len(response)} bytes) from STM2: {[hex(b) for b in response]}")
-            #        else:
-            #            self.get_logger().warn(f"⚠️ Incomplete echo from STM2 ({len(response)} bytes)")
+                    ##New debugging unpacking
+                    if len(response2) == 16:
+                        if response2[0] == ord('P'):
+                            try:
+                                p1_2,p2_2,p3_2 = struct.unpack('<iii', response2[1:13])
+                                m1_moving_2 = response2[13]
+                                m2_moving_2 = response2[14]
+                                m3_moving_2 = response2[15]
+
+                                self.motors_idle2 = not (m1_moving_2 or m2_moving_2 or m3_moving_2)
+                                
+                                self.get_logger().info(f"\n2 - position 1: {p1_2}\n2 - position 2: {p2_2}\n2 - position 3: {p3_2}"
+                                                       f"\nM1_2 is moving: {m1_moving_2}"
+                                                       f"\nM2_2 is moving: {m2_moving_2}"
+                                                       f"\nM3_2 is moving: {m3_moving_2}")
+                            except struct.error as e:
+                                self.get_logger().warn(f"Failed to unpack positions: {e}")
+                        else:
+                            self.get_logger().warn(f"Unexpected header in response: {response2[0]}, raw={list(response2)}")
+                    else:
+                        self.get_logger().warn(f"Incomplete response from STM1 ({len(response2)} bytes)")
 
         except Exception as error:
             self.get_logger().error(f"Error in serial communication: {error}")
 
-    
-
-    #def send_motor_commands(self):
-    #    """ Send motor commands to both serial devices """
-    #    try:
-    #        if self.releasing and (time.time() - self.release_start_time > 2.0):
-    #            self.relay_commands = bytearray([10, 10])
-    #            self.releasing = False
-#
-    #        data1 = self.get_data_to_stm32(front=True)
-    #        #data2 = self.get_data_to_stm32(front=False)
-#
-    #        if data1:
-    #            #with self.lock1:
-    #            #    self.ser1.write(data1)
-    #            #    time.sleep(0.005)
-    #            #    self.ser1.read(19)
-#
-    #            with self.lock1:
-    #                self.ser1.write(data1)
-    #                self.get_logger().info(f"→ Sent to STM32: {list(data1)}")
-    #                time.sleep(0.5)
-    #                response = self.ser1.readline().decode(errors='ignore').strip()
-    #                if response:
-    #                    self.get_logger().info(f"← Received from STM32: {response}")
-#
-    #        #if data2:
-    #        #    with self.lock2:
-    #        #        self.ser2.write(data2)
-    #        #        time.sleep(0.005)
-    #        #        self.ser2.read(19)
-    #    except Exception as error:
-    #        self.get_logger().error(f"Error in serial communication: {error}")
 
     def get_data_to_stm32(self, stm=1):
 
@@ -291,18 +261,15 @@ class MotorSerialNode(Node):
             data = self.header + bytes([reset_int]) + bytes([save_int]) + motors + self.relay_commands + self.terminators + bytearray([0])
         else:
             # STM2 -> RR + RL DC motors
-            motors = self.motor_commands[6:9]
-            data = self.header + bytes([reset_int]) + bytes([save_int]) + motors + self.relay_commands + self.terminators + bytearray([0, 0, 0, 0, 0])
-
-        
-        #data = self.header + bytes([reset_int]) + bytes([save_int]) + motors + self.relay_commands + self.terminators + bytearray([0])
-
-        #if len(data) != 16:  # Adjust length if needed
-        #    return data  # or None if strict
-        #return data
-        # Make sure data has 16 bytes
-        #if len(data) < 16:
-        #    data += bytearray([0] * (16 - len(data)))
+            motors_list = [
+                self.motor_commands[1],
+                self.motor_commands[1],
+                self.motor_commands[1],
+            ] + list(self.motor_commands[12:16])
+            motors = bytearray(motors_list)
+            data = self.header + bytes([reset_int]) + bytes([save_int]) + motors + self.relay_commands + self.terminators + bytearray([0])
+            #motors = self.motor_commands[6:9]
+            #data = self.header + bytes([reset_int]) + bytes([save_int]) + motors + self.relay_commands + self.terminators + bytearray([0, 0, 0, 0, 0])
 
         return data
 
